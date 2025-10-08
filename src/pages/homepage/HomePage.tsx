61a26b0a7634a61bc442c63d67fd6ce5d417bca1
Joker <puebloseko@gmail.com>
2025-10-08 14:18:22 +0200

    Basic fetch to main url. Shows the 50 first pokemon with imagen and name

--- src/pages/homepage/HomePage.tsx
+++ src/pages/homepage/HomePage.tsx
@@ -1,35 +1,31 @@
-import { useNavigate } from "react-router-dom";
 import Header from "../../components/homepage/header/Header";
+import RenderZone from "../../components/homepage/renderZone/RenderZone";
 
 function HomePage() {
- const navigate = useNavigate(); 
-function goo(){
- 
-  navigate("/details");
-}
+
+
 
 
   return (
-    <div className="flex flex-col h-full p-2 gap-2">
+    <div className="flex flex-col h-full p-2 gap-1">
       
-      <div className=" h-[10%] flex items-center justify-center">
+      <div className=" h-[8%] flex items-center justify-center">
        <Header/>
       </div>
 
       {/* Fila 2 - Selectores de tags */}
-      <div className="border border-gray-400 h-[10%] overflow-x-auto flex items-center">
+      <div className="border border-gray-400 h-[8%] overflow-x-auto flex items-center">
         Tags
       </div>
 
       {/* Fila 3 - Barra de búsqueda */}
-      <div className="border border-gray-400 h-[10%] flex items-center justify-center">
+      <div className="border border-gray-400 h-[8%] flex items-center justify-center">
         Search
       </div>
 
       {/* Fila 4 - Zona de Pokémon */}
       <div className="border border-gray-400 flex-1 overflow-auto">
-        Zona Pokémon
-        <button onClick={goo}>go</button>
+       <RenderZone/>
       </div>
     </div>
   );
