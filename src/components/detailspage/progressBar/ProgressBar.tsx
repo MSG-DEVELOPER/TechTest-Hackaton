fbb74cd82923e07a79dcde7d456245c4167916cf
Joker <puebloseko@gmail.com>
2025-10-09 07:42:04 +0200

    Details page done. Stats done. Spinner done. It miss readme, tests and analisis

---
+++ src/components/detailspage/progressBar/ProgressBar.tsx
@@ -0,0 +1,15 @@
+// BarraProgreso.jsx
+function ProgressBar({ valor }) {
+  const ancho = Math.min(valor, 110); // límite de 100px por si el valor es muy alto
+
+  return (
+    <div className="w-[110px] h-2 bg-red-300 rounded">
+      <div
+        className="h-full bg-blue-500 rounded"
+        style={{ width: `${ancho}px` }}
+      ></div>
+    </div>
+  );
+}
+
+export default ProgressBar;
--- src/pages/detailspage/DetailsPage.tsx
+++ src/pages/detailspage/DetailsPage.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { getPokemonDetails } from "../../api/pokemonApiDetails";
 import { getTypeColor } from "../../utils/getTypeColor";
+import ProgressBar from "../../components/detailspage/progressBar/ProgressBar";
+import Spinner from "../../components/common/Spinner";
 
 interface PokemonDetails {
   id: number;
@@ -40,21 +42,21 @@ function DetailsPage() {
     fetchDetails();
   }, [id]);
 
-  if (!pokemon) {
-    return (
-      <div className="flex items-center justify-center h-screen">
-        <p className="text-sm font-semibold">Loading...</p>
-      </div>
-    );
-  }
+ if (!pokemon) {
+  return (
+    <div className="flex items-center justify-center w-[340px] h-[400px] mx-auto shadow">
+      <Spinner />
+    </div>
+  );
+}
 
   return (
-    <div className="flex flex-col items-center justify-start w-[340px] h-[400px] mx-auto shadow p-1 space-y-1 text-sm -mt-1">
+    <div className="flex flex-col items-center justify-start w-[340px] h-[400px] mx-auto shadow p-1 space-y-1 text-sm -mt-1 bg-gray-300">
       {/* Fila 1: Botón Back y nombre */}
       <div className="w-full p-1 px-3 flex justify-between items-center">
         <button
           onClick={handleBack}
-          className="px-2 py-1 bg-gray-200 rounded text-xs hover:bg-gray-300"
+          className="px-2 py-1 bg-gray-200 rounded text-xs hover:bg-blue-300"
         >
           ⏮
         </button>
@@ -88,44 +90,44 @@ function DetailsPage() {
 
       {/* Fila 5: Height & Weight */}
       <div className="w-full flex justify-center gap-4 text-xs">
-        <span>Height: {pokemon.height} m</span>
-        <span>Weight: {pokemon.weight} kg</span>
+        <span className="font-bold">Height: {pokemon.height} m</span>
+        <span className="font-bold">Weight: {pokemon.weight} kg</span>
       </div>
 
       {/* Fila 6: Zona inferior */}
-      <div className="w-full border flex-1 flex flex-col p-1 text-xs rounded">
+      <div className="w-full  flex-1 flex flex-col  text-xs bg-gray-200">
         {/* Fila 1: Título */}
-        <div className="w-full border-b pb-1">
+        <div className="w-full pb-1">
           <span className="font-bold">STATS</span>
         </div>
 
         {/* Fila 2: Stats propiamente dicho */}
-        <div className="flex flex-1 mt-1 gap-2">
+        <div className="flex flex-1 mt-1 ">
           {/* Columna izquierda */}
-          <div className="flex-1 flex flex-col gap-1 border p-1">
+          <div className="flex-1 flex flex-col gap-2 p-1">
             {/* Fila 1 */}
-            <div className="flex justify-between border p-1 rounded">
+            <div className="flex justify-between  p-1 items-center">
               <span>HP: {pokemon.stats.hp}</span>
-              <span>slot</span>
+              <span><ProgressBar valor={pokemon.stats.hp}/> </span>
             </div>
             {/* Fila 2 */}
-            <div className="flex justify-between border p-1 rounded">
-              <span>Attack: {pokemon.stats.attack}</span>
-              <span>slot</span>
+            <div className="flex justify-between  p-1  items-center">
+              <span>AT: {pokemon.stats.attack}</span>
+              <span><ProgressBar valor={pokemon.stats.attack}/></span>
             </div>
           </div>
 
           {/* Columna derecha */}
-          <div className="flex-1 flex flex-col gap-1 border p-1">
+          <div className="flex-1 flex flex-col gap-2  p-1">
             {/* Fila 1 */}
-            <div className="flex justify-between border p-1 rounded">
-              <span>Defense: {pokemon.stats.defense}</span>
-              <span>slot</span>
+            <div className="flex justify-between  p-1 items-center">
+              <span>DF: {pokemon.stats.defense}</span>
+              <span><ProgressBar valor={pokemon.stats.defense}/></span>
             </div>
             {/* Fila 2 */}
-            <div className="flex justify-between border p-1 rounded">
-              <span>Speed: {pokemon.stats.speed}</span>
-              <span>slot</span>
+            <div className="flex justify-between  p-1 items-center">
+              <span>SP: {pokemon.stats.speed}</span>
+              <span><ProgressBar valor={pokemon.stats.speed}/></span>
             </div>
           </div>
         </div>
